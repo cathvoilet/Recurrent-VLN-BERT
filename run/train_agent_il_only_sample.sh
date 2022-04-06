66bde67bdd65da27fc53ea5686416d69adc71d70
Lingjun <cathvoilet@wireless-10-104-163-57.umd.edu>
2022-04-06 11:57:04 -0400

    il only for training

---
+++ run/train_agent_il_only_sample.sh
@@ -0,0 +1,39 @@
+#!/bin/bash --login
+#SBATCH --job-name=train_vlnbert_agent_il-rs150
+#SBATCH --output=slurm_logs/train_vlnbert_agent_il-rs150.out
+#SBATCH --error=slurm_logs/train_vlnbert_agent_il-rs150.err
+#SBATCH --time=6-00:00:00
+#SBATCH --partition=dpart
+#SBATCH --qos=default
+#SBATCH --mem=32gb
+#SBATCH --gres=gpu:p6000
+
+set -x
+module add cuda/8.0.44 cudnn/v5.1
+
+DOCKER=/vulcanscratch/lzhao/docker/vlnbert.sif
+
+name=VLNBERT-train-il-rs150
+
+flag="--vlnbert prevalent
+      --test_only 0
+      --train listener
+      --features places365
+      --maxAction 15
+      --batchSize 16
+      --feedback sample
+      --lr 1e-5
+      --iters 300000
+      --seed 150
+      --train_sampling 0.9
+      --optim adamW
+      --mlWeight 0.20
+      --maxInput 80
+      --angleFeatSize 128
+      --featdropout 0.4
+      --dropout 0.5"
+
+mkdir -p snap/$name
+
+singularity exec --bind /vulcanscratch:/vulcanscratch --nv $DOCKER python3 -u r2r_src/train.py $flag --name $name
+
